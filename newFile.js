810656bd78b3374e9ce500b167ebccf08cadbe19
aishwaryacr5 <152845538+aishwaryacr5@users.noreply.github.com>
2023-12-04 13:09:21 +0530

    Add files via upload
    Recipe book with AI recommendations project using css and html

---
+++ newFile.js
@@ -0,0 +1,8 @@
+searchBtn.addEventListener("click", () => {
+    let userInp = document.getElementById("user-inp").value;
+    if (userInp.length == 0) {
+        result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+    } else {
+        fetch(url = "http://127.0.0.1:5500/chickenbiryani.html" + userInp);
+    }
+});
---
+++ searchpage.js
@@ -0,0 +1,13 @@
+//Initial References
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+searchBtn.addEventListener("click", () => {
+  let userInp = document.getElementById("user-inp").value;
+  if (userInp.length == 0) {
+    result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+  } else {
+    fetch(url = "http://127.0.0.1:5500/chickenbiryani.html" + userInp)
+  }
+});
